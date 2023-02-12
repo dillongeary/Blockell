c7af51681afee1bfeb4bec29e3530a58753c5f30
Dillon Geary <13gearyd@gmail.com>
2023-02-12 14:29:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# COMP3200 Project
+
+A web-app IDE, written in Type Script using the Blockly Library.<br/>
+A tool where users can make Haskell like code using a Block-Based Visual Programming Language.
+
+Electronics and Computer Science<br/>
+Faculty of Physical Sciences and Engineering<br/>
+University of Southampton
+
+A project submitted for the award of MEng Computer Science
---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import * as Blockly from "blockly"
+  import type {ToolboxDefinition} from "blockly/core/utils/toolbox";
+  import {Toolbox} from "blockly";
+  import {onMount} from "svelte";
+
+  const toolbox:ToolboxDefinition = {
+    "kind":"flyoutToolbox",
+    "contents": [
+      {
+        "kind":"block",
+        "type":"controls_if"
+      }
+    ]
+  };
+
+  onMount(async () => {
+    const blocklyDiv = document.getElementById("blocklyDiv")
+    const workspace = Blockly.inject(blocklyDiv, {
+      toolbox:toolbox
+    });
+  });
+
+</script>
+
+<main>
+  <div id="blocklyDiv" class="blocklyWorkspace"></div>
+</main>
+
+<style>
+  .blocklyWorkspace {
+    width:50vw;
+    height:80vh;
+    background-color:white;
+  }
+</style>
