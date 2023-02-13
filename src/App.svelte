1fa810e24bcc0a71ecaf6c8075cb56c0742deae1
Dillon Geary <13gearyd@gmail.com>
2023-02-13 22:23:29 +0000

    modified to add basic lambda calculus blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,114 @@
 <script lang="ts">
   import * as Blockly from "blockly"
-  import type {ToolboxDefinition} from "blockly/core/utils/toolbox";
-  import {Toolbox} from "blockly";
   import {onMount} from "svelte";
+  import type {ToolboxDefinition} from "blockly/core/utils/toolbox";
 
-  const toolbox:ToolboxDefinition = {
-    "kind":"flyoutToolbox",
+  const toolbox = {
+    "kind":"categoryToolbox",
     "contents": [
       {
-        "kind":"block",
-        "type":"controls_if"
+        "kind": "category",
+        "name": "Lambda",
+        "colour":"290",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "lamda_calculus"
+          },
+          {
+            "kind": "block",
+            "type": "lambda_starter"
+          },
+          {
+            "kind": "block",
+            "type": "variable"
+          }
+        ]
+      },
+      {
+        "kind": "category",
+        "name": "Variables",
+        "colour":"290",
+        "custom": "VARIABLE"
       }
     ]
   };
 
+  //Block Definitions
+
+  Blockly.Blocks['lamda_calculus'] = {
+    init: function() {
+      this.appendValueInput("NAME")
+              .setCheck("String")
+              .appendField("λ")
+              .appendField(new Blockly.FieldVariable("var"), "NAME")
+              .appendField("→");
+      this.setInputsInline(false);
+      this.setOutput(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks['lambda_starter'] = {
+    init: function() {
+      this.appendValueInput("NAME")
+              .setCheck(null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks['variable'] = {
+    init: function() {
+      this.appendDummyInput()
+              .appendField(new Blockly.FieldVariable("var"), "NAME");
+      this.setOutput(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  const options = {
+    toolbox : toolbox,
+    collapse : true,
+    comments : true,
+    disable : true,
+    maxBlocks : Infinity,
+    trashcan : true,
+    horizontalLayout : false,
+    toolboxPosition : 'start',
+    css : true,
+    media : 'https://blockly-demo.appspot.com/static/media/',
+    rtl : false,
+    scrollbars : true,
+    sounds : true,
+    oneBasedIndex : true,
+    zoom : {
+      controls : true,
+      wheel : true,
+      startScale : 1,
+      maxScale : 3,
+      minScale : 0.3,
+      scaleSpeed : 1.2
+    }
+  };
+
+
   onMount(async () => {
+
     const blocklyDiv = document.getElementById("blocklyDiv")
-    const workspace = Blockly.inject(blocklyDiv, {
-      toolbox:toolbox
-    });
+
+    const workspace = Blockly.inject(blocklyDiv, options);
+
+    const workspaceBlocks = document.getElementById("workspaceBlocks");
+
+    Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
   });
+
 
 </script>
 
@@ -29,8 +118,8 @@
 
 <style>
   .blocklyWorkspace {
-    width:50vw;
-    height:80vh;
+    width:100vw;
+    height:100vh;
     background-color:white;
   }
 </style>
