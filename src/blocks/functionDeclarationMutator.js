986f788ffe72996dc31b07efa7bc148859a276fd
Dillon Geary <13gearyd@gmail.com>
2023-04-24 11:39:38 +0100

    Added tuple function type?

--- src/blocks/functionDeclarationMutator.js
+++ src/blocks/functionDeclarationMutator.js
@@ -89,7 +89,7 @@ function getBlockFromString (workspace, input) {
         let tupleBlock = workspace.newBlock("function_create_with_tuple")
         let connection = tupleBlock.getInput("TUPLETYPE").connection
         for (const type of types) {
-            let typeBlock = getBlockFromString(type)
+            let typeBlock = getBlockFromString(workspace,type)
             connection.connect(typeBlock.previousConnection);
             connection = typeBlock.nextConnection;
         }
