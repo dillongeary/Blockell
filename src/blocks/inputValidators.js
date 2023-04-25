2177d1d148020f59acd2e98f706625c27acc6565
Dillon Geary <13gearyd@gmail.com>
2023-04-25 16:49:55 +0100

    added camel case validation for multiword inputs in variable block

--- src/blocks/inputValidators.js
+++ src/blocks/inputValidators.js
@@ -1,6 +1,7 @@
 export let integerValidator = function () {
     this.getField("VAL").setValidator(function (value) {
-        return value.replace(/[^0-9]/g,"")
+        value = value.replace(/[^0-9-.]/g,"")
+        return Math.round(value);
     })
 }
 export let characterValidator = function () {
@@ -13,7 +14,33 @@ export let characterValidator = function () {
 }
 export let variableValidator = function () {
     this.getField("VAL").setValidator(function (value) {
-        value = value[0].toLowerCase() + value.slice(1);
-        return value.replace(/[^0-9a-zA-Z_-]/g,"")
+        value = value.replace(/[^0-9a-zA-Z_\- ]/g,"");
+        let newValue = ""
+        let space = false;
+        for (const char of value) {
+            if (char === " ") {
+                space = true;
+            } else if (space === true) {
+                newValue+= char.toUpperCase()
+                space = false;
+            } else {
+                newValue+= char;
+            }
+        }
+        value = newValue
+
+        var first = value[0];
+        value = value.slice(1);
+
+        while (first && first.replace(/[^a-z]/g,"") === "") {
+            if (!(first.replace(/[^A-Z]/g,"") === "")) {
+                first = first.toLowerCase()
+            } else {
+                first = value[0]
+                value = value.slice(1)
+            }
+        }
+        if (!(first)) {first = ""}
+        return first + value;
     })
 }
